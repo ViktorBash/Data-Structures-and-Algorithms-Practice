28d219ef38c3281328da05e07646de8d0c030747
Viktor Basharkevich <basharkevichv@gmail.com>
2020-05-15 22:33:32 -0400

    Added more functions to binary tree

--- Basic Data Structures/Binary Tree/binary_tree.py
+++ Basic Data Structures/Binary Tree/binary_tree.py
@@ -34,6 +34,7 @@ class Node:  # Node for tree, with value, and then pointer to left and right
 #     def size(self):
 #         return len(self.items)
 
+
 # My queue, more logical ordering with items in last place being at the end of the queue
 class Queue:
     def __init__(self):
@@ -62,6 +63,31 @@ class Queue:
             return self.items[0].value
 
 
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def get_stack(self):
+        return self.items
+
+    def is_empty(self):
+        return self.items == []
+
+    def peek(self):  # Get the first item in the stack, (the one that will be popped first)
+        if not self.is_empty():
+            return self.items[-1]
+
+    def __len__(self):
+        return self.items.__len__()
+
+
 class BinaryTree:  # class to make the tree
     def __init__(self, root):  # Make a root node
         self.root = Node(root)
@@ -75,6 +101,8 @@ class BinaryTree:  # class to make the tree
             return self.post_order_print(self.root, "")
         elif traversal_type.lower() == "levelorder":
             return self.level_order_print(self.root)
+        elif traversal_type.lower() == "reverselevelorder":
+            return self.reverse_level_order_print(self.root)
         else:
             print("Traversal type not supported")
             return False
@@ -123,9 +151,64 @@ class BinaryTree:  # class to make the tree
 
         return traversal
 
+    def reverse_level_order_print(self, start):
+        if start is None:
+            return
+
+        queue = Queue()
+        queue.enqueue(start)
+        stack = Stack()
+        traversal = ""
+        while len(queue) > 0:
+            node = queue.dequeue()
+            stack.push(node)
+
+            if node.right:
+                queue.enqueue(node.right)
+            if node.left:
+                queue.enqueue(node.left)
+
+        while len(stack) > 0:
+            node = stack.pop()
+            traversal += str(node.value) + "-"
+
+        return traversal
+
+    def height(self, node):
+        if node is None:
+            return -1
+        left_height = self.height(node.left)
+        right_height = self.height(node.right)
+
+        return 1 + max(left_height, right_height)
+
+    def size(self):
+        if self.root is None:
+            return 0
+
+        stack = Stack()
+        stack.push(self.root)
+        size = 1
+
+        while stack:
+            node = stack.pop()
+            if node.left:
+                size += 1
+                stack.push(node.left)
+            if node.right:
+                size += 1
+                stack.push(node.right)
+
+        return size
+
+    def size_recursive(self, node):
+        if node is None:
+            return 0
+        return 1 + self.size_recursive(node.left) + self.size_recursive(node.right)
+
 
 def main():
-    pass
+
     # Demonstrating creating and traversing a tree
     # tree = BinaryTree(1)
     # tree.root.left = Node(2)
@@ -144,7 +227,11 @@ def main():
     tree.root.right = Node(3)
     tree.root.left.left = Node(4)
     tree.root.left.right = Node(5)
-    print(tree.print_tree("levelorder"))
+    # print(tree.print_tree("levelorder"))
+    # print(tree.print_tree("reverselevelorder"))
+    # print(tree.height(tree.root))
+    print(tree.size())
+    print(tree.size_recursive(tree.root))
 
 
 if __name__ == "__main__":
