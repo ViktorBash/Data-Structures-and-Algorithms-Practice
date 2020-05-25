0eb29af962caf5e36811009e13876d9e29ec2e5c
Viktor Basharkevich <basharkevichv@gmail.com>
2020-05-25 02:52:21 -0400

    Added first version of deletion to priority queue heap

--- Data Structures/Priority Queues and Heaps/priority_queue_heap.py
+++ Data Structures/Priority Queues and Heaps/priority_queue_heap.py
@@ -6,7 +6,7 @@ This will be a minheap (good for Dijkstra's algorithm)
 
 class PriorityQueue:
     def __init__(self):
-        self.items = [None, 3, 5, 4, 10, 8, 6]
+        self.items = [None]
 
     def parent(self, pos):
         if pos == 0:
@@ -60,12 +60,86 @@ class PriorityQueue:
 
             # Iterate through (change parent)
             parent_item = self.parent(parent_item)
-            print(self.items)
 
     # Takes in 2 indices to swap values
     def swap_nodes(self, num1, num2):
         self.items[num1], self.items[num2] = self.items[num2], self.items[num1]
 
+    def delete(self):
+        # Swap nodes and delete
+        self.swap_nodes(1, -1)
+        del self.items[-1]
+
+        parent = 1
+        left_child = self.left_child(parent)
+        right_child = self.right_child(parent)
+
+        parent_val = self.items[parent]
+        print(parent_val)
+        try:
+            left_child_val = self.items[self.left_child(parent)]
+        except IndexError:
+            left_child_val = None
+        try:
+            right_child_val = self.items[self.right_child(parent)]
+        except IndexError:
+            right_child_val = None
+
+        print(f"parent val: {parent_val} left_child_val {left_child_val} right_child_val {right_child_val}")
+        print(f"right_child_index {right_child} left_child_index {left_child}")
+        break_loop = False
+        while not break_loop:
+            print(self.items)
+            print(parent)
+            if not right_child_val and not left_child_val:
+                break_loop = True
+                break
+
+            elif not right_child_val and left_child_val < parent_val:
+                self.swap_nodes(parent, left_child)
+                parent = self.left_child(parent)
+            elif not left_child_val and right_child_val < parent_val:
+                self.swap_nodes(parent, right_child)
+                parent = self.right_child(parent)
+            elif left_child_val and right_child_val:
+                if left_child_val >= right_child_val and right_child_val < parent_val:
+                    self.swap_nodes(parent, right_child)
+                    parent = self.right_child(parent)
+                elif left_child_val < right_child_val:
+                    self.swap_nodes(parent, left_child)
+                    parent = self.left_child(parent)
+            else:
+                break_loop = True
+                break
+
+            # Declare left child and val, right child and val
+            right_child = self.right_child(parent)
+            left_child = self.left_child(parent)
+            try:
+                left_child_val = self.items[self.left_child(parent)]
+            except:
+                left_child_val = None
+            try:
+                right_child_val = self.items[self.right_child(parent)]
+            except:
+                right_child_val = None
+
+
+
+
+
+
+
 
 q = PriorityQueue()
+q.insert(3)
+q.insert(5)
+q.insert(4)
+q.insert(10)
+q.insert(8)
+q.insert(6)
 q.insert(1)
+print(q.items)
+q.delete()
+print(q.items)
+
