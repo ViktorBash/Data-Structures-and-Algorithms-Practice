0e7822ad44210ca654b2b9381eb4feeaa68f76df
Viktor Basharkevich <basharkevichv@gmail.com>
2020-05-25 21:43:04 -0400

    Finished dijkstra implementation for shortest path

--- Data Structures/Graphs/Dijkstra's Algorithm/dijkstra_implementation.py
+++ Data Structures/Graphs/Dijkstra's Algorithm/dijkstra_implementation.py
@@ -41,6 +41,7 @@ class Graph:
         self.num_vertices = 0
         self.vert_distance = {}
         self.vert_prev = {}
+        self.finished = {}
 
     def __iter__(self):
         return iter(self.vert_dict.values())
@@ -69,27 +70,46 @@ class Graph:
         return str(self.vert_dict)
 
     def dijkstra(self, start_node="S"):
-        for vert_name, vert in self.vert_dict.items():
-            self.vert_distance[vert_name] = 999
-            self.vert_prev[vert_name] = None
-        self.vert_distance["S"] = 0
+        # Setup before we actually do the algorithm
+        for vert_name, vert in self.vert_dict.items():  # Iterate through vertices
+            self.vert_distance[vert_name] = 999  # Set distance of current vertex to 999
+            self.vert_prev[vert_name] = None  # Set prev_vertex to None
+            self.finished[vert_name] = False  # No vertices have finished being discovered, so this makes it all False
+        self.vert_distance["S"] = 0  # Distance from S to itself is 0, so we put this in
 
-        self.dijkstra_util("S")
+        self.dijkstra_util("S")  # Call the actual algorithm with start vertex of "S"
 
-    def dijkstra_util(self, cur_node):
-        start_vert = self.get_vertex(cur_node)
-        for key, value in start_vert.adjacent.items():
-            if self.vert_distance[key] > value:
-                self.vert_prev[key] = cur_node
-                self.vert_distance[key] = value
+    def dijkstra_util(self, cur_node):  # Actual algorithm
+        while False in self.finished.values():  # While we have not explored every single vertex
+            start_vert = self.get_vertex(cur_node)  # Get the start_vertex object from the name
+            for key, value in start_vert.adjacent.items():  # Iterate through start_vertex neighbors
+                if self.vert_distance[key] > self.vert_distance[cur_node] + value:  # If shorter distance
+                    self.vert_prev[key] = cur_node  # Set prev_node to cur_node
+                    self.vert_distance[key] = value + self.vert_distance[cur_node]  # Update value (weight + value of
+                    # start vert)
+                # print(f"cur node {cur_node} neighbor {key} val {self.vert_distance[key]} prev {self.vert_prev[key]}")
+            # print(" ")
+            self.finished[cur_node] = True
+            next_node = None  # Temp var, will be changed
+            next_node_dist = 999  # Temp var, will be changed
+            # Iterate over neighbors and choose one that has the shortest distance
+            for key, value in self.vert_dict.items():
+                if self.vert_distance[key] < next_node_dist and self.finished[key] is False:
+                    next_node = key
+                    next_node_dist = self.vert_distance[key]
+            # print(next_node)
+            # for key, value in self.vert_distance.items():
+            #     print(self.vert_distance[key])
+            #     if self.vert_distance[key] < next_node_dist:
+            #         next_node = key
+            # print(next_node)
+            print(self.vert_distance)
+            print(self.vert_prev)
+            print(self.finished)
+            print("")
+            print(next_node)
 
-        next_node = None
-        next_node_dist = 999
-        for key, value in self.vert_distance.items():
-            if value < next_node_dist:
-                next_node = key
-        print(next_node)
-        self.dijkstra_util(next_node)
+            self.dijkstra_util(next_node)  # Put vert we selected into algorithm recursively
 
 
 if __name__ == "__main__":
